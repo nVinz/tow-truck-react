a8b446fc206544d3b74924bd5b7c83a06be18b59
Arkady <arkon.f2@gmail.com>
2020-03-18 11:08:54 +0300

    + _redirects

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import './App.css';
 import User from './user/User'
 import Admin from './admin/Admin'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 
 class App extends React.Component {
   render() {
-    var isAdmin = window.location.pathname.replace('/', '') === 'admin' ? true : false;
+    /*var isAdmin = window.location.pathname.replace('/', '') === 'admin' ? true : false;
     const body = isAdmin ? <section><Admin/></section> : <section><User/></section>;
     return (
       <div className="App">
         {body}
       </div>
-    );
-
-    /*return (
-      <div className="App">
-        <Route exact path="/" component={User}/>
-        <Route exact path="/admin" component={Admin}/>
-      </div>
     );*/
+
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={User}/>
+          <Route exact path="/admin/" component={Admin}/>
+        </Switch>
+      </div>
+    );
   }
 }
 
